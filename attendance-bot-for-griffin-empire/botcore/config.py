74e480726cd48847fd667ad2aec4ea395bc81070
DragonTaki <synasaivaltos@gmail.com>
2025-04-18 05:38:44 +0800

    ✅ First valid commit from correct project root

---
+++ attendance-bot-for-griffin-empire/botcore/config.py
@@ -0,0 +1,16 @@
+# ----- ----- ----- -----
+# config.py
+# For Albion Online "Griffin Empire" Guild only
+# Do not distribute or modify
+# Author: DragonTaki (https://github.com/DragonTaki)
+# Create Date: 2025/04/18
+# Update Date: 2025/04/18
+# Version: v1.0
+# ----- ----- ----- -----
+
+GUILD_NAME = ["Griffin Empire"]
+INTERVALS = [7, 14, 28]
+MIN_GP = 50
+CSV_HEADERS = ["Player", "7DaysAttendance", "14DaysAttendance", "28DaysAttendance"]
+CACHE_EXPIRY_HOURS = 8
+MAX_CSV_VERSIONS = 3
